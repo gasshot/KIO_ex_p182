36d9416f1da934fad1f4bf4418e44b8dfbd7a134
인오 강 <kanginoh@gmail.com>
2023-03-16 21:25:45 +0900

    aa

--- KIO_ex_p182_01/MainApp.cs
+++ KIO_ex_p182_01/MainApp.cs
@@ -7,9 +7,9 @@ namespace KIO_ex_p182_01
     {
         static void Main(string[] args)
         {
-            for(int c = 0; c<5;c++)
-                {
-                for (int t=0;t<=c;t++)
+            for (int c = 0; c < 5; c++)
+            {
+                for (int t = 0; t <= c; t++)
                 {
                     Console.Write("*");
                 }
--- KIO_ex_p182_02/MainApp.cs
+++ KIO_ex_p182_02/MainApp.cs
@@ -7,6 +7,14 @@ namespace KIO_ex_p182_02
     {
         static void Main(string[] args)
         {
+            for (int c = 0; c < 5; c++)
+            {
+                for (int t = 0; t < 5-c; t++) 
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
 
         }
     }
