cffa8a1ad903633b798b0a3330285b97cbe7aaec
인오 강 <kanginoh@gmail.com>
2023-03-16 21:08:32 +0900

    초기 커밋.

---
+++ KIO_ex_p182/MainApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace KIO_ex_p182
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+            for (int c = 0; c < 5; c++)
+            {
+                for (int t = 0; t <= c; t++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
