08ee840f566ade22c99690a91a2d6efc127b3cb6
인오 강 <kanginoh@gmail.com>
2023-03-16 21:21:43 +0900

    a

--- KIO_ex_p182_01/MainApp.cs
+++ KIO_ex_p182_01/MainApp.cs
@@ -1,16 +1,15 @@
 ﻿using System;
 
 
-namespace KIO_ex_p182
+namespace KIO_ex_p182_01
 {
     class MainApp
     {
         static void Main(string[] args)
         {
-
-            for (int c = 0; c < 5; c++)
-            {
-                for (int t = 0; t <= c; t++)
+            for(int c = 0; c<5;c++)
+                {
+                for (int t=0;t<=c;t++)
                 {
                     Console.Write("*");
                 }
--- KIO_ex_p182_01/Properties/AssemblyInfo.cs
+++ KIO_ex_p182_01/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("KIO_ex_p182")]
+[assembly: AssemblyTitle("KIO_ex_p182_01")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KIO_ex_p182")]
+[assembly: AssemblyProduct("KIO_ex_p182_01")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("f3e43528-875f-4657-9bc5-4b6c28508c7f")]
+[assembly: Guid("70d591de-01a6-48f8-8ce9-d16f939e2dad")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
